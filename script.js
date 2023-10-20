ec55e5d5498abfb5487cf78ca4166c8f7632eebf
Jack-Hunter14 <jack.hunter14@education.nsw.gov.au>
2023-10-20 17:24:39 +1100

    fixed array pointer issue and finished insertion sort

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ document.getElementById("ScriptThisDIV").innerHTML = "Debug the innerHTML"; //Ou
 console.log("Debug the console log"); //Output to the console (Open dev tools and look at the console)
  
 let myArray = randomiseArray();
+console.log("original: " + myArray);
 highestValue(myArray);
 bubbleSort(myArray);
 insertionSort(myArray);
+
 
 let swapArray = [3, 7]
 
@@ -28,15 +30,20 @@ function randomiseArray() {
     return randomArray;
 }
 
-function bubbleSort (sortArray) {
+function bubbleSort (tempArray) {
+    sortArray = [];
+    for (w = 0; w < tempArray.length; w++) {
+        sortArray[w] = tempArray[w];
+    }
     endpoint = sortArray.length - 1;
-    startpoint = 0;
-    loops = 0;
     for (i = 0; i < sortArray.length; i++) {
         correct = 0;
-        for (n = startpoint; n < endpoint; n++) {
+        for (n = 0; n < endpoint; n++) {
             if (n == endpoint - 1) {
                 endpoint--;
+                if (correct == endpoint - 1) {
+                    correct--;
+                }
             }
             if (sortArray[n] > sortArray[n + 1]) {
                 sortArray[n] += sortArray[n + 1];
@@ -49,33 +56,59 @@ function bubbleSort (sortArray) {
                     n = endpoint;
                 }
             }
-            loops++;
         }
     }
     console.log("bubble sort: " + sortArray);
-    console.log("bubble loops: " + loops);
 }
 
 function highestValue (searchArray) {
     currentValue = searchArray[0];
+    currentIndex = 0;
     for (i = 0; i < searchArray.length; i++) {
         if (currentValue < searchArray[i + 1]) {
             currentValue = searchArray[i + 1];
+            currentIndex = i + 1;
         }
     }
     console.log("highest value: " + currentValue);
+    console.log("index: " + currentIndex);
 }
 
-function insertionSort (sortArray) {
-    loops = 0;
-    for (i = 1; i < sortArray.length; i++) {
-        if (sortArray[i] > sortArray[i + 1]) {
-            sortArray[i] += sortArray[i + 1];
-            sortArray[i + 1] = sortArray[i] - sortArray[i + 1];
-            sortArray[i] -= sortArray[i + 1];
+/*
+START
+SET myArray = [2, 3, 5]
+highestValue (myArray)
+FUNCTION highestValue (searchArray)
+    SET currentValue = searchArray[0]
+    SET currentIndex = 0
+    FOR COUNT from 0 to length of searchArray
+        IF currentValue < searchArray[i + 1]
+            SET currentValue = searchArray[i + 1]
+            SET currentIndex = i + 1
+        END IF
+    COUNT + 1
+    END COUNT LOOP
+    RETURN currentValue
+    RETURN currentIndex
+END FUNCTION
+END
+*/
+
+function insertionSort (tempArray) {
+    sortArray = [];
+    for (w = 0; w < tempArray.length; w++) {
+        sortArray[w] = tempArray[w];
+    }
+    endpoint = sortArray.length;
+    for (n = 0; n < sortArray.length; n++) {
+        for (i = 0; i < endpoint - 1; i++) {
+            if (sortArray[i] > sortArray[i + 1]) {
+                sortArray[i] += sortArray[i + 1];
+                sortArray[i + 1] = sortArray[i] - sortArray[i + 1];
+                sortArray[i] -= sortArray[i + 1];
+            }
         }
-        loops++;
+        endpoint--;
     }
     console.log("insertion sort: " + sortArray);
-    console.log("insertion loops: " + loops);
 }
