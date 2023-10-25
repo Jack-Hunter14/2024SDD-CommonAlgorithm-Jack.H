4018ac72c5d3927e0723da7a461b9d0c6ac94787
Jack-Hunter14 <jack.hunter14@education.nsw.gov.au>
2023-10-26 09:36:25 +1100

    created calculator folder

---
+++ Calculator/script.js
@@ -0,0 +1,45 @@
+//alert("Hello, world"); //Output to windows alert box
+document.getElementById("ScriptThisDIV").innerHTML = "Debug the innerHTML"; //Output to the DOM (Document Object Module) with the id="ScriptThisDIV"
+console.log("Debug the console log"); //Output to the console (Open dev tools and look at the console)
+ 
+/*
+START
+SET num1 to GET keyboard input
+SET pressed to GET calc key
+SET num2 to GET keyboard input
+SET result
+IF pressed = plus
+    SET result to addNum(num1, num2)
+ELSE IF pressed = minus
+    SET result to minNum(num1, num2)
+ELSE IF pressed = multiply
+    SET result to multNum(num1, num2)
+ELSE IF pressed = divide
+    SET result to divNum(num1, num2)
+ELSE IF pressed = power
+    SET result to powNum(num1, num2)
+END IF
+DISPLAY result
+END
+
+START SUBROUTINE addNum (a, b)
+RETURN a + b
+END SUBROUTINE
+
+START SUBROUTINE minNum (a, b)
+RETURN a - b
+END SUBROUTINE
+
+START SUBROUTINE multNum (a, b)
+RETURN a * b
+END SUBROUTINE
+
+START SUBROUTINE divNum (a, b)
+RETURN a / b
+END SUBROUTINE
+
+START SUBROUTINE powNum (a, b)
+RETURN a ^ b
+END SUBROUTINE
+*/
+
